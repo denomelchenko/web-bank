ab9a3a7ed5efdbf8ce11e9826872695c2dac0e78
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-02-23 15:47:38 +0100

    Implement web bank user details

---
+++ src/main/java/com/domelchenko/webbank/config/WebBankUserDetails.java
@@ -0,0 +1,42 @@
+package com.domelchenko.webbank.config;
+
+import com.domelchenko.webbank.model.Customer;
+import com.domelchenko.webbank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class WebBankUserDetails implements UserDetailsService {
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public WebBankUserDetails(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String userName, password = null;
+        List<GrantedAuthority> authorities = null;
+        List<Customer> customer = customerRepository.findByEmail("username");
+        if (customer.size() == 0) {
+            throw new UsernameNotFoundException("User details for " + username + " user do not found.");
+        } else {
+            userName = customer.get(0).getEmail();
+            password = customer.get(0).getPassword();
+            authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+        }
+        return new User(userName, password, authorities);
+    }
+}
