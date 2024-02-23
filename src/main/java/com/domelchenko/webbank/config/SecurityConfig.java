4e8ee1fcf7c6778662bfec87634a836ed911147f
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-02-23 08:47:51 +0100

    Add spring security config

---
+++ src/main/java/com/domelchenko/webbank/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.domelchenko.webbank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
