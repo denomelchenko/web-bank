bf02d886c427eec8a549358943a968477ea9bd58
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-02-23 07:22:17 +0100

    Intial commit

---
+++ src/main/java/com/domelchenko/webbank/WebBankApplication.java
@@ -0,0 +1,13 @@
+package com.domelchenko.webbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBankApplication.class, args);
+    }
+
+}
