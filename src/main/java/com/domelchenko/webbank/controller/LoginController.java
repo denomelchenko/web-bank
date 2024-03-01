12ca732fba057d0eba7331f24b0d49809792d063
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-01 16:13:17 +0200

    Update password encoder to BCrypt

--- src/main/java/com/domelchenko/webbank/controller/LoginController.java
+++ src/main/java/com/domelchenko/webbank/controller/LoginController.java
@@ -1,7 +1,34 @@
 package com.domelchenko.webbank.controller;
 
+import com.domelchenko.webbank.model.Customer;
+import com.domelchenko.webbank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+    private final CustomerRepository customerRepository;
+
+    public LoginController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        try {
+            customerRepository.save(customer);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body("User successfully created");
+
+        } catch (Exception exception) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occurred due to " + exception);
+        }
+    }
 }
