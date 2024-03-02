2559a05fc3db3b7a9199fc38f3dd6a09e3097e46
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-02 14:02:51 +0200

    Add account entity

---
+++ src/main/java/com/domelchenko/webbank/model/Account.java
@@ -0,0 +1,41 @@
+package com.domelchenko.webbank.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "accounts", indexes = {
+        @Index(name = "customer_id", columnList = "customer_id")
+})
+public class Account {
+    @Id
+    @Column(name = "account_number", nullable = false)
+    private Integer accountNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @Column(name = "account_type", nullable = false, length = 100)
+    private String accountType;
+
+    @Column(name = "branch_address", nullable = false, length = 200)
+    private String branchAddress;
+
+    @Column(name = "create_dt")
+    private LocalDate createDt;
+
+    @OneToMany(mappedBy = "accountNumber")
+    private Set<AccountTransaction> accountTransactions = new LinkedHashSet<>();
+
+}
