df1276d526f5a07ba5fb0c77da8cdb455200a1c7
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-17 15:49:06 +0100

    update security config

--- src/main/java/com/domelchenko/webbank/config/ProjectSecurityConfig.java
+++ src/main/java/com/domelchenko/webbank/config/ProjectSecurityConfig.java
@@ -40,7 +40,11 @@ public class ProjectSecurityConfig {
                 )
                 .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/myAccount").hasRole("USER")
+                        .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/loans").hasRole("USER")
+                        .requestMatchers("/myCards").hasRole("USER")
+                        .requestMatchers("/user").authenticated()
                         .requestMatchers("/notices", "/contact", "/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
