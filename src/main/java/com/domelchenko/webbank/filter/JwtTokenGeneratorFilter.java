7393a7f8291b1e1539abaca762cb9998c618f6f9
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-03-23 15:45:09 +0100

    Implement JwtTokenGenereatorFilter

---
+++ src/main/java/com/domelchenko/webbank/filter/JwtTokenGeneratorFilter.java
@@ -0,0 +1,59 @@
+package com.domelchenko.webbank.filter;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.*;
+
+public class JwtTokenGeneratorFilter extends OncePerRequestFilter {
+
+    @Value("${security.secret}")
+    private String securityHeader;
+    
+    @Value("${security.header}")
+    private String securitySecret;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            Key key = Keys.hmacShaKeyFor(securitySecret.getBytes(StandardCharsets.UTF_8));
+            String jwt = Jwts.builder()
+                    .issuer("Bank")
+                    .subject("JWT Token")
+                    .claim("username", authentication.getName())
+                    .claim("authorities", populateAuthorities(authentication.getAuthorities()))
+                    .issuedAt(new Date())
+                    .expiration(new Date(new Date().getTime() + 3000))
+                    .signWith(key)
+                    .compact();
+            response.setHeader(securityHeader, jwt);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> collection) {
+        Set<String> authoritiesSet = new HashSet<>();
+        for (GrantedAuthority authority : collection) {
+            authoritiesSet.add(authority.getAuthority());
+        }
+        return String.join(",", authoritiesSet);
+    }
+}
