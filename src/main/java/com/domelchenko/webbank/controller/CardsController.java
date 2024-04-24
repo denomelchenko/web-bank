3f0bf7865691f09084f7d7ff1f9f6d5db903192c
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-24 10:31:45 -0700

    Update CardsController

--- src/main/java/com/domelchenko/webbank/controller/CardsController.java
+++ src/main/java/com/domelchenko/webbank/controller/CardsController.java
@@ -1,12 +1,23 @@
 package com.domelchenko.webbank.controller;
 
+import com.domelchenko.webbank.model.Cards;
+import com.domelchenko.webbank.repository.CardsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CardsController {
-    @GetMapping("/cards")
-    public String getCards() {
-        return "0 cards for now";
+    private final CardsRepository cardsRepository;
+
+    public CardsController(CardsRepository cardsRepository) {
+        this.cardsRepository = cardsRepository;
+    }
+
+    @GetMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestParam int id) {
+        return cardsRepository.findByCustomerId(id);
     }
 }
