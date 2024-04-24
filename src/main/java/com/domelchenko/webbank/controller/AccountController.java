012c80ab6084f6913df405d98d75ea51b1ae6f00
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-24 10:31:07 -0700

    Update AccountController

--- src/main/java/com/domelchenko/webbank/controller/AccountController.java
+++ src/main/java/com/domelchenko/webbank/controller/AccountController.java
@@ -1,13 +1,19 @@
 package com.domelchenko.webbank.controller;
 
+import com.domelchenko.webbank.model.Accounts;
+import com.domelchenko.webbank.repository.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AccountController {
+    @Autowired
+    private AccountsRepository accountsRepository;
+
     @GetMapping("/myAccount")
-    public String getInfo() {
-        return "MY ACC";
+    public Accounts getAccountDetails(@RequestParam int id) {
+        return accountsRepository.findByCustomerId(id);
     }
 }
