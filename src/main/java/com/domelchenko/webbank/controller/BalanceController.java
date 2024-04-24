5074aea6885e72c2cc9f634924adcc5d98ad8704
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-24 10:31:24 -0700

    Update BalanceController

--- src/main/java/com/domelchenko/webbank/controller/BalanceController.java
+++ src/main/java/com/domelchenko/webbank/controller/BalanceController.java
@@ -1,4 +1,20 @@
 package com.domelchenko.webbank.controller;
 
+import com.domelchenko.webbank.model.AccountTransactions;
+import com.domelchenko.webbank.repository.AccountTransactionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 public class BalanceController {
+    @Autowired
+    private AccountTransactionsRepository accountTransactionsRepository;
+
+    @GetMapping("/myBalance")
+    public List<AccountTransactions> getBalanceDetails(@RequestParam int id) {
+        return accountTransactionsRepository.
+                findByCustomerIdOrderByTransactionDtDesc(id);
+    }
 }
