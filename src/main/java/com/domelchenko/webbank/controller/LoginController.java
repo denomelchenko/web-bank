1cf2cb7d584df84a43127238e6c903bdb56210fe
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-24 10:32:46 -0700

    Update LoginController

--- src/main/java/com/domelchenko/webbank/controller/LoginController.java
+++ src/main/java/com/domelchenko/webbank/controller/LoginController.java
@@ -2,37 +2,58 @@ package com.domelchenko.webbank.controller;
 
 import com.domelchenko.webbank.model.Customer;
 import com.domelchenko.webbank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+import java.util.List;
 
 @RestController
 public class LoginController {
-    private final CustomerRepository customerRepository;
 
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private CustomerRepository customerRepository;
 
-    public LoginController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
-        this.customerRepository = customerRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        Customer savedCustomer = null;
+        ResponseEntity response = null;
         try {
-            customer.setPwd(passwordEncoder.encode(customer.getPwd()));
-            customerRepository.save(customer);
-            return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body("User successfully created");
-
-        } catch (Exception exception) {
-            return ResponseEntity
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
+            customer.setCreateDt(new Date(System.currentTimeMillis()).toLocalDate());
+            savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user details are successfully registered");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("An exception occurred due to " + exception);
+                    .body("An exception occured due to " + ex.getMessage());
         }
+        return response;
+    }
+
+    @RequestMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication) {
+        List<Customer> customers = customerRepository.findByEmail(authentication.getName());
+        if (customers.size() > 0) {
+            return customers.get(0);
+        } else {
+            return null;
+        }
+
     }
 }
