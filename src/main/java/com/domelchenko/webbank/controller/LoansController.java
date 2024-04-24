e9254e0732de135e670c56a12503a770372e5cea
Denys Omelchenko <denysomelchenko.dev@gmail.com>
2024-04-24 10:32:27 -0700

    Update LoansController

--- src/main/java/com/domelchenko/webbank/controller/LoansController.java
+++ src/main/java/com/domelchenko/webbank/controller/LoansController.java
@@ -1,4 +1,26 @@
 package com.domelchenko.webbank.controller;
 
+import com.domelchenko.webbank.model.Loans;
+import com.domelchenko.webbank.repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 public class LoansController {
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @GetMapping("/myLoans")
+    @PostAuthorize("hasRole('USER')")
+    public List<Loans> getLoanDetails(@RequestParam int id) {
+        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
+        if (loans != null ) {
+            return loans;
+        }else {
+            return null;
+        }
+    }
 }
